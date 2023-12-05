74710d17280c0052002d1e41f3a839f14910470a
k-f-m <55965735+k-f-m@users.noreply.github.com>
2023-12-05 14:52:24 +0100

    Add data model for migrations

---
+++ BlazorToDo/Data/Models/Todo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorToDo.Data.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Task { get; set; } = string.Empty;
+
+        public DateTime? DueDate { get; set; }
+
+        [Required]
+        public DateTime CreationTimestamp { get; set; } = DateTime.Now;
+    }
+}
