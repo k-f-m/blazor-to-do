9b35b1cfa09d2990ce61361fc5b4207e492f2a3b
k-f-m <55965735+k-f-m@users.noreply.github.com>
2023-12-05 15:18:52 +0100

    Add ApplicationDbContext class to interact with database
    This commit adds an ApplicationDbContext class to the BlazorToDo.Data namespace. The ApplicationDbContext class inherits from the DbContext class provided by the Microsoft.EntityFrameworkCore namespace, and is used to interact with the database in .NET applications. The class includes a DbSet property called TodoList, which represents a collection of Todo objects in the database.

---
+++ BlazorToDo/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorToDo.Data
+{
+    /// <summary>
+    /// This class is used to interact with the database in .NET applications.
+    /// </summary>
+    /// <remarks>
+    /// This class inherits from the `DbContext` class provided by the `Microsoft.EntityFrameworkCore` namespace.
+    /// </remarks>
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        /// <summary>
+        /// Gets or sets a collection of `Todo` objects in the database.
+        /// </summary>
+        public DbSet<BlazorToDo.Data.Models.Todo> TodoList { get; set; } = default!;
+    }
+}
